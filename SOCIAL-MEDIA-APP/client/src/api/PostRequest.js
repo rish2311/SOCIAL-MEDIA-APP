a2ce61434a6567024d0ae48ade07318cfa5fce67
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-01-03 18:12:21 +0530

    fixed minor issues

--- SOCIAL-MEDIA-APP/client/src/api/PostRequest.js
+++ SOCIAL-MEDIA-APP/client/src/api/PostRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: "http://localhost:8000"})
 
 export const getTimelinePosts=(id)=>API.get(`/posts/${id}/time`)
 export const likePost =(id, userId) => API.put(`post/${id}/like`,{userId:userId})
--- SOCIAL-MEDIA-APP/client/src/api/UploadRequest.js
+++ SOCIAL-MEDIA-APP/client/src/api/UploadRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: "http://localhost:8000"})
 
 export const uploadImage = (data)=> API.post('/upload',data)
 
